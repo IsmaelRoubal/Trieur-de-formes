28c4ff4f6242d36631c8c0826e848a1f6c166ffd
SharKass <90391884+SharKass@users.noreply.github.com>
2022-12-01 15:06:17 +0100

    Commit
    Nouvelle version du tapis

--- Assets/conveyor.cs
+++ Assets/conveyor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class conveyor : MonoBehaviour
 {
     // Start is called before the first frame update
-    float speed = 0.05F;
+    float speed = 0.5F;
 
     void Start()
     {
